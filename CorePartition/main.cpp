25b74b1838c90a9787d654abaf32e217d85de13d
Gustavo Campos <lgustavocampos@gmail.com>
2019-07-14 23:11:01 +0100

    Initial Commit

---
+++ CorePartition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CorePartition
+//
+//  Created by GUSTAVO CAMPOS on 14/07/2019.
+//  Copyright © 2019 GUSTAVO CAMPOS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
