120f54e5a9d4c50e66556e177a5c4f98ccc6ba96
Gustavo Campos <lgustavocampos@gmail.com>
2019-10-29 23:09:26 +0000

    New demos, now including a new object for Scrolling text or presenting it statically with big letters and numbers

--- CorePartition/Resources/ThrermalCameraDemo/ThrermalCameraDemo.ino
+++ CorePartition/Resources/ThrermalCameraDemo/ThrermalCameraDemo.ino
@@ -94,99 +94,6 @@ int CLK = 10; //SS
 
 // Functions
 
-
-static const uint64_t byteImages[] PROGMEM = {
-  0x0000000000000000,
-  0x00180018183c3c18,
-  0x0000000012246c6c,
-  0x0036367f367f3636,
-  0x000c1f301e033e0c,
-  0x0063660c18336300,
-  0x006e333b6e1c361c,
-  0x0000000000030606,
-  0x00180c0606060c18,
-  0x00060c1818180c06,
-  0x0000663cff3c6600,
-  0x00000c0c3f0c0c00,
-  0x060c0c0000000000,
-  0x000000003f000000,
-  0x000c0c0000000000,
-  0x000103060c183060,
-  0x003e676f7b73633e,
-  0x003f0c0c0c0c0e0c,
-  0x003f33061c30331e,
-  0x001e33301c30331e,
-  0x0078307f33363c38,
-  0x001e3330301f033f,
-  0x001e33331f03061c,
-  0x000c0c0c1830333f,
-  0x001e33331e33331e,
-  0x000e18303e33331e,
-  0x000c0c00000c0c00,
-  0x060c0c00000c0c00,
-  0x00180c0603060c18,
-  0x00003f00003f0000,
-  0x00060c1830180c06,
-  0x000c000c1830331e,
-  0x001e037b7b7b633e,
-  0x6666667e66663c00,
-  0x3e66663e66663e00,
-  0x3c66060606663c00,
-  0x3e66666666663e00,
-  0x7e06063e06067e00,
-  0x0606063e06067e00,
-  0x3c66760606663c00,
-  0x6666667e66666600,
-  0x3c18181818183c00,
-  0x1c36363030307800,
-  0x66361e0e1e366600,
-  0x7e06060606060600,
-  0xc6c6c6d6feeec600,
-  0xc6c6e6f6decec600,
-  0x3c66666666663c00,
-  0x06063e6666663e00,
-  0x603c766666663c00,
-  0x66361e3e66663e00,
-  0x3c66603c06663c00,
-  0x18181818185a7e00,
-  0x7c66666666666600,
-  0x183c666666666600,
-  0xc6eefed6c6c6c600,
-  0xc6c66c386cc6c600,
-  0x1818183c66666600,
-  0x7e060c1830607e00,
-  0x006e333e301e0000,
-  0x003b66663e060607,
-  0x001e3303331e0000,
-  0x006e33333e303038,
-  0x001e033f331e0000,
-  0x000f06060f06361c,
-  0x1f303e33336e0000,
-  0x006766666e360607,
-  0x001e0c0c0c0e000c,
-  0x1e33333030300030,
-  0x0067361e36660607,
-  0x001e0c0c0c0c0c0e,
-  0x00636b7f7f330000,
-  0x00333333331f0000,
-  0x001e3333331e0000,
-  0x0f063e66663b0000,
-  0x78303e33336e0000,
-  0x000f06666e3b0000,
-  0x001f301e033e0000,
-  0x00182c0c0c3e0c08,
-  0x006e333333330000,
-  0x000c1e3333330000,
-  0x00367f7f6b630000,
-  0x0063361c36630000,
-  0x1f303e3333330000,
-  0x003f260c193f0000,
-  0x00380c0c070c0c38,
-  0x000c0c0c0c0c0c0c,
-  0x00070c0c380c0c07,
-  0x0000000000003b6e
-};
-
 LedControl lc=LedControl(DIN,CLK,CS,2);
 
 
@@ -196,13 +103,6 @@ void setLocation (uint16_t nY, uint16_t nX)
     uint8_t nLen = snprintf ((char*) szTemp, sizeof(szTemp), "\033[%u;%uf", nY, nX);
 
     Serial.write (szTemp, nLen);
-    
-    //Serial.flush ();
-    //Serial.print ("\033[");
-    //Serial.print (nY);
-    //Serial.print (";");
-    //Serial.print (nX);
-    //Serial.print ("f");
 }
 
 
@@ -213,13 +113,6 @@ void setColor (const uint8_t nFgColor, const uint8_t nBgColor)
     uint8_t nLen = snprintf ((char*) szTemp, sizeof(szTemp), "\033[%u;%um", nFgColor + 30, nBgColor + 40);
 
     Serial.write (szTemp, nLen);
-    
-    //Serial.flush ();
-    //Serial.print ("\033[1;");
-    //Serial.print (nFgColor + 30);
-    //Serial.print (";");
-    //Serial.print (nBgColor + 40);
-    //Serial.print ("m");
 }
 
 
@@ -253,26 +146,6 @@ void reverseColor ()
 }
 
 
-void printByte(byte character [])
-{
-  int i = 0;
-  for(i=0;i<8;i++)
-  {
-    lc.setColumn(0,i,character[i]);
-  }
-}
-
-void printScrollBytes(uint8_t nDevice, uint64_t charLeft, uint64_t charRight, uint8_t nOffset)
-{
-  int i = 0;
-  for(i=0;i<8;i++)
-  {        
-      lc.setColumn(nDevice,i, (((uint8_t*) &charLeft) [i] << (8-nOffset) | ((uint8_t*) &charRight) [i] >> nOffset));
-  }
-
-}
-
-
 void Delay (uint64_t nSleep)
 {
     uint32_t nMomentum =  millis();
@@ -285,23 +158,147 @@ void Delay (uint64_t nSleep)
 }
 
 
-volatile uint32_t nCount = 10;
-
-char szTemp [40];
-
-uint64_t getImage (unsigned int nIndex)
+const uint64_t byteImages[] PROGMEM =
 {
-    uint16_t nImagesItens = sizeof (byteImages) / sizeof (byteImages[0]);
-    uint64_t nBuffer= 0xAA;
+    0x0000000000000000, 0x00180018183c3c18, 0x0000000012246c6c, 0x0036367f367f3636, 0x000c1f301e033e0c,
+    0x0063660c18336300, 0x006e333b6e1c361c, 0x0000000000030606, 0x00180c0606060c18, 0x00060c1818180c06,
+    0x0000663cff3c6600, 0x00000c0c3f0c0c00, 0x060c0c0000000000, 0x000000003f000000, 0x000c0c0000000000,
+    0x000103060c183060, 0x003e676f7b73633e, 0x003f0c0c0c0c0e0c, 0x003f33061c30331e, 0x001e33301c30331e,
+    0x0078307f33363c38, 0x001e3330301f033f, 0x001e33331f03061c, 0x000c0c0c1830333f, 0x001e33331e33331e,
+    0x000e18303e33331e, 0x000c0c00000c0c00, 0x060c0c00000c0c00, 0x00180c0603060c18, 0x00003f00003f0000,
+    0x00060c1830180c06, 0x000c000c1830331e, 0x001e037b7b7b633e, 0x6666667e66663c00, 0x3e66663e66663e00,
+    0x3c66060606663c00, 0x3e66666666663e00, 0x7e06063e06067e00, 0x0606063e06067e00, 0x3c66760606663c00,
+    0x6666667e66666600, 0x3c18181818183c00, 0x1c36363030307800, 0x66361e0e1e366600, 0x7e06060606060600,
+    0xc6c6c6d6feeec600, 0xc6c6e6f6decec600, 0x3c66666666663c00, 0x06063e6666663e00, 0x603c766666663c00,
+    0x66361e3e66663e00, 0x3c66603c06663c00, 0x18181818185a7e00, 0x7c66666666666600, 0x183c666666666600,
+    0xc6eefed6c6c6c600, 0xc6c66c386cc6c600, 0x1818183c66666600, 0x7e060c1830607e00, 0x001e06060606061e,
+    0x00406030180c0603, 0x001e18181818181e, 0x0000000063361c08, 0x003f000000000000, 0x0000000000180c06,
+    0x7c667c603c000000, 0x3e66663e06060600, 0x3c6606663c000000, 0x7c66667c60606000, 0x3c067e663c000000,
+    0x0c0c3e0c0c6c3800, 0x3c607c66667c0000, 0x6666663e06060600, 0x3c18181800180000, 0x1c36363030003000,
+    0x66361e3666060600, 0x1818181818181800, 0xd6d6feeec6000000, 0x6666667e3e000000, 0x3c6666663c000000,
+    0x06063e66663e0000, 0xf0b03c36363c0000, 0x060666663e000000, 0x3e603c067c000000, 0x1818187e18180000,
+    0x7c66666666000000, 0x183c666600000000, 0x7cd6d6d6c6000000, 0x663c183c66000000, 0x3c607c6666000000,
+    0x3c0c18303c000000, 0x00380c0c070c0c38, 0x0c0c0c0c0c0c0c0c, 0x00070c0c380c0c07, 0x0000000000003b6e
+ };
+
+const int byteImagesLen = sizeof(byteImages)/8;
+
+      
+class TextScroller
+{
+private:
     
-    nIndex = nIndex > nImagesItens ? nImagesItens - nIndex : nIndex;
-
-
-    memcpy_PF(&nBuffer, byteImages + nIndex, sizeof (uint64_t));
+     int nNumberDigits = 15;
+     uint8_t nOffset = 0;
+     int  nIndex = nNumberDigits * (-1);
+     uint8_t nSpeed;
     
-    return nBuffer;
-}
+    uint64_t getLetter (int nIndex, const char* pszMessage, uint16_t nMessageLen)
+    {
+        int nCharacter = nIndex > nMessageLen || nIndex < 0 ? ' ' : pszMessage [nIndex];
+        
+        return getImage (nCharacter - ' ');
+    }
 
+    uint64_t getImage (int nIndex)
+    {
+        uint64_t nBuffer= 0xAA;
+
+        nIndex = nIndex > byteImagesLen ||  nIndex < 0  ? 0 : nIndex;
+
+        memcpy_P (&nBuffer, byteImages + nIndex, sizeof (uint64_t));
+        
+        return nBuffer;
+    }
+
+    void printScrollBytes(uint16_t  nLocY, uint16_t nLocX, uint16_t nDigit, const uint64_t charLeft, const uint64_t charRight, uint8_t nOffset)
+    {
+        int i = 0;
+          
+        for(i=0;i<8;i++)
+        {
+            printRow (nLocY, nLocX, nDigit, i, (((uint8_t*) &charLeft) [i] << (8-nOffset) | ((uint8_t*) &charRight) [i] >> nOffset));
+        }
+    }
+
+    
+protected:
+    
+    virtual void printRow (uint16_t nLocY, uint16_t nLocX, uint16_t nDigit, uint8_t nRowIndex, uint8_t nRow)
+    {
+        static char nLine [9] = "        ";
+        int8_t nOffset = 8;
+
+        //Serial.print (nRow, BIN);
+        
+        while (--nOffset >= 0)
+        {
+           nLine [7-nOffset] = nRow & 1 != 0 ? '#' : ' ';
+           nRow >>= 1;
+        }
+
+        setLocation (nLocY + nRowIndex, nLocX + (nDigit * 8));
+        Serial.print (nLine);
+    }
+
+public:
+    
+    TextScroller (int nNumberDigits, uint8_t nSpeed) :
+        nNumberDigits (nNumberDigits),
+        nOffset (0),
+        nSpeed (nSpeed)
+    {
+       nIndex = nSpeed == 0 ? 0 : nNumberDigits * (-1);
+    }
+    
+    
+    void show (int nLocY, int nLocX, const char* pszMessage, const uint16_t nMessageLen)
+    {
+        uint8_t nCount;
+       
+        if (nSpeed > 0 && nSpeed % 8 == 0) nSpeed++;
+        
+        if (nSpeed > 0) do
+        {
+          if (nOffset >= 7)
+          {
+              nIndex = nIndex + 1 > (int) nMessageLen ? (int) nNumberDigits * (-1) : nIndex + 1;
+              nOffset = 0;
+       
+          }
+          else
+          {
+              nOffset = (int) nOffset + (nSpeed  % 8);
+          }
+        } while (nOffset >= 7);
+
+              
+        for (nCount=0; nCount < nNumberDigits; nCount++)
+        {
+            printScrollBytes (nLocY, nLocX, nCount, getLetter(nIndex + 1, pszMessage, nMessageLen), getLetter(nIndex, pszMessage, nMessageLen), (uint8_t) nOffset);
+          
+            nIndex =  (int) nIndex + 1;
+        }
+          
+         nIndex = (int) nIndex - (nCount - (nSpeed / 8));
+     
+    }
+};
+
+
+class MatrixTextScroller : public TextScroller 
+{
+protected:
+
+    void printRow (uint16_t nLocY, uint16_t nLocX, uint16_t nDigit, uint8_t nRowIndex, uint8_t nRow) override
+    {
+        lc.setColumn(nDigit, nRowIndex, nRow);
+    }
+
+public:
+
+    using TextScroller::TextScroller;
+};
 
 
 
@@ -315,6 +312,9 @@ void Thread1 ()
     uint16_t nImagesItens = sizeof (byteImages) / sizeof (byteImages[0]);
     //setCoreNice (100);
 
+    MatrixTextScroller matrixTextScroller (2, 1);
+    
+    const char szMessage[] = "CorePartition! :) works!";
     
     while (1)
     {
@@ -333,21 +333,7 @@ void Thread1 ()
          
           start = millis();
 
-          
-          printScrollBytes (1, getImage ( ((a+1 >= nImagesItens) ? 0 : a+1) ), getImage (a), nOffset);
-          b = (a+1 >= nImagesItens) ? 0 : a+1;
-          printScrollBytes (0, getImage ( ((b+1 >= nImagesItens) ? 0 : b+1) ), getImage (b), nOffset);
-          
-          if (nOffset + 1 >= 8)
-          {
-              nOffset = 0;
-              a = (a+1 >= nImagesItens ) ? 0 : a+1;
-          }
-          else
-          {
-              nOffset++;
-          }
-          
+          matrixTextScroller.show (0, 0, szMessage, sizeof (szMessage)-1);
           
         //digitalWrite (2, LOW);
         //Delay (2000);
@@ -357,6 +343,7 @@ void Thread1 ()
 }
 
 
+float fMin = 1000, fMax = 0; 
 
 
 void Thread2 ()
@@ -392,39 +379,31 @@ void Thread2 ()
           Serial.print (" from ");
           Serial.print (CorePartition_GetPartitionAllocatedMemorySize ());
           Serial.println ("]\n");
-
-         float fmin = 1000, fmax = 0; 
       
+          fMin = 1000, fMax = 0; 
+          
+          for(int i=AMG88xx_PIXEL_ARRAY_SIZE; i > 0; i--)
+          { 
+            fMin = MIN (fMin, pixels[i-1]);
+            fMax = MAX (fMax, pixels[i-1]);      
+          }
+
+          CorePartition_Yield ();
+          
+          setLocation (1,1);
+
+          resetColor ();
           
           for(int i=AMG88xx_PIXEL_ARRAY_SIZE; i > 0; i--)
           {
-            
-            fmin = MIN (fmin, pixels[i-1]);
-            fmax = MAX (fmax, pixels[i-1]);      
-          }
-
-          Serial.print ("\e[1:1H");
-
-          for(int i=AMG88xx_PIXEL_ARRAY_SIZE; i > 0; i--)
-          {
-            Serial.print ("\x1b[48;5;");
-            //Serial.print (nHotColor [map (pixels[i-1], fmin, fmin + 8, 0, sizeof (nHotColor)-1)]);
-            Serial.print (map (pixels[i-1], fmin, fmin + 10, 232, 255 ));
-            Serial.print ("m  \e[0m");
+            Serial.print ("\033[48;5;");
+            //Serial.print (nHotColor [map (pixels[i-1], fMin, fMin + 8, 0, sizeof (nHotColor)-1)]);
+            Serial.print (map (pixels[i-1], fMin, fMax, 232, 255 ));
+            Serial.print ("m  \033[0m");
             
             if( (i-1)%8 == 0 ) Serial.println();
           }
-      
-          Serial.println();
           
-          Serial.print ("Min: ");
-          Serial.print (fmin);
-          Serial.print (" Max: ");
-          Serial.println (fmax);
-      
-          Serial.println();
-          Serial.println();
-        
           Serial.flush();
               
       
@@ -432,7 +411,7 @@ void Thread2 ()
         //digitalWrite (3, LOW);
         //Delay (1000);
         CorePartition_Yield ();
-        //digitalWrite (3, HIGH);
+        //digitalWrite (3, HIGH); 
         
     }
 }
@@ -468,6 +447,42 @@ void Thread3 ()
         //digitalWrite (4, HIGH);
     }
 }
+
+
+
+void Thread4 ()
+{    
+    char szMessage [20]=""; 
+    uint8_t nSize;
+
+    TextScroller textScroller (16, 0);
+
+    while (1)
+    {
+        nSize = snprintf (szMessage, sizeof (szMessage)-1, "Min:%dc Max:%dc", (int) fMin, (int) fMax);
+
+        setColor (1, 0);
+        
+        textScroller.show (10, 1, szMessage, nSize);
+
+        resetColor ();
+        
+        Serial.println();
+        
+        Serial.print ("Min: ");
+        Serial.print (fMin);
+        Serial.print (" Max: ");
+        Serial.println (fMax);
+    
+        Serial.println();
+        Serial.println();
+
+        
+        CorePartition_Yield ();
+    }
+}
+
+
 
 
 static uint64_t getTimeTick()
@@ -515,7 +530,10 @@ void setup()
 
     delay (1000);
 
+    resetColor ();
+    clearConsole ();
     hideCursor ();
+    
     //pinMode (2, OUTPUT);
     //pinMode (3, OUTPUT);
     //pinMode (4, OUTPUT);
@@ -528,16 +546,18 @@ void setup()
     //pinMode(nPinInput, INPUT_PULLUP);
     //attachInterrupt(digitalPinToInterrupt(nPinInput), CorePartition_YieldPreemptive, CHANGE);
 
-    CorePartition_Start(3);
+    CorePartition_Start (4);
     
     CorePartition_SetCurrentTimeInterface(getTimeTick);
     CorePartition_SetSleepTimeInterface(sleepTick);
 
     CreatePartition(Thread1, 100, 100);
     
-    CreatePartition(Thread2, 100, 50);
+    CreatePartition(Thread2, 100, 25);
 
-    CreatePartition(Thread3, 100, 100);
+    CreatePartition(Thread3, 100, 50);
+
+    CreatePartition(Thread4, 100, 200);
 }
 
 
