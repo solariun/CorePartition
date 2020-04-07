bf6a022107946e895b9121fcac02f6f674773b65
Gustavo Campos <lgustavocampos@gmail.com>
2020-04-07 23:21:52 +0100

    adding auto arduino cli compile at wifidotmatrix

--- CorePartition/Resources/WiFiDotMatrixDisply/Terminal.hpp
+++ CorePartition/Resources/WiFiDotMatrixDisply/Terminal.hpp
@@ -43,7 +43,7 @@ Class Terminal
 
     virtual ~Terminal ();
 
-
+    
 
     protected:
     
---
+++ CorePartition/Resources/arduino-cli-upload.sh
@@ -0,0 +1,1 @@
+arduino-cli upload --port /dev/cu.wchusbserial1461140  --fqbn esp8266:esp8266:nodemcuv2 WiFiDotMatrixDisply
